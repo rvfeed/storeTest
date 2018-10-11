8068270e92148a1290d551481309392b17ecc032
Raja <akhil406@gmail.com>
2018-10-11 09:53:47 +0530

    refracted code

--- storeTest/src/app/app.constants.ts
+++
@@ -1,8 +0,0 @@
-import {InjectionToken} from "@angular/core";
-export let APP_CONSTANTS = new InjectionToken("app.constants");
-export type DefaultValues = {
-    RATINGS: Number[];
-    GENRES: String[];
-}
-export const RATINGS: Number[] = [1,2,3,4,5,7,8,9,10];
-export const GENRES: String[] = ["Action", "Crime", "Comedy", "Horror", "Romance", "Sci-Fi", "Animation"];
